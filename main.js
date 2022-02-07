46a93384c3df05d9f95be176171b3a08e986cc0d
Aarav414 <84316227+Aarav414@users.noreply.github.com>
2022-02-07 19:15:35 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+    PoseNet = ml5.poseNet(video, modelLoaded);
+PoseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized!");
+}
+
+function draw(){
+    background("#FFFF00");
+}
+
+function gotPoses(results){
+if(results.length > 0){
+    console.log(results);
+}
+}
